52612eabed280e8a1927ad20c2e931395a8f3017
Jonathan <jonathanalmeidadk2@gmail.com>
2026-05-12 22:47:51 -0300

    dicas adicionadas ao seis_numeros.py

--- Estudos práticos/curso em video2/seis_numeros.py
+++ Estudos práticos/curso em video2/seis_numeros.py
@@ -1,8 +1,8 @@
-soma = 0
-cont = 0
-for c in range(1, 7):
+soma = 0 # variável para somar os números digitados
+cont = 0 # variável para contar os números digitados
+for c in range(1, 7): #crie um intervalo de 1 a 6
     n = int(input(f"Digite o {c}º número: "))
-    if n % 2==0:
-        soma += n
-        cont += 1
+    if n % 2==0: # diferencia os ímpares dos pares, só quero os pares neste caso
+        soma += n # soma = soma + n
+        cont += 1 # cont = cont + 1
 print(f"Você informou {cont} números PARES e a soma foi {soma}. ")
