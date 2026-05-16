603a91df2b4400be2fce681e65c6f7e23defc447
Jonathan <jonathanalmeidadk2@gmail.com>
2026-05-15 23:46:20 -0300

    tabuada com for corrigida

--- Estudos práticos/chatgpt/tabuada_for.py
+++ Estudos práticos/chatgpt/tabuada_for.py
@@ -2,15 +2,20 @@ num = int(input('Digite um número e veja sua tabuada: '))
 
 pares = 0
 impares = 0
-
+soma = 0
 for t in range(0, 11):
+    
+    resultado = num * t 
 
-    print(f'{num} x {t} = {num*t}')
+    print(f'{num} x {t} = {resultado}')
 
-    if t % 2 == 0:
+    soma += resultado
+
+    if resultado % 2 == 0:
         pares += 1
     else:
         impares += 1
 
+print(f'Soma dos resultados: {soma}')
 print(f'Resultados pares: {pares}')
 print(f'Resultados ímpares: {impares}')
