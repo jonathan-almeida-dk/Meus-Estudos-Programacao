c840debcdd1d5d33655d177b1f6e270e8bc3e816
Jonathan <jonathanalmeidadk2@gmail.com>
2026-05-25 22:54:05 -0300

    correção

--- Estudos práticos/chatgpt/media3.py
+++ Estudos práticos/chatgpt/media3.py
@@ -2,38 +2,51 @@ aprovados = 0
 reprovados = 0
 excelentes = 0
 recuperacao = 0
-while True:
-  nome = input('Digite o nome do aluno: ')
-  n1 = float(input('Digite a 1ª nota: '))
-  n2 = float(input('Digite a 2ª nota: '))
-  n3 = float(input('Digite a 3ª nota: '))
-  media = (n1 + n2 + n3) / 3
 
-  print(f'Nome do aluno: {nome}')
-  print(f'\nMédia do aluno: {media:.1f}')
-  if media < 5:
-    reprovados += 1
-    print('Reprovado')
-  elif media < 7:
-    recuperacao += 1
-    print('Recuperacao')
-  elif media < 9:
-    aprovados += 1
-    print('Aprovado')
-  else:
-    excelentes += 1
-    print('Você foi excelente!')
-    
-  print(f'Excelentes: {excelentes}')
-  print(f'\nAprovados: {aprovados}')
-  print(f'\nReprovados: {reprovados}')
-  print(f'\nRecuperação: {recuperacao}')
-  cont = input('\nDeseja continuar? (s/n) ').upper().lower()
-  if cont == 's':
-    continue
-  elif cont == 'n':
-    break
-  else:
-    print('Opção inválida! ', end='')
-    print('Tente novamente')
-    continue
+while True:
+
+    nome = input('Digite o nome do aluno: ')
+
+    n1 = float(input('Digite a 1ª nota: '))
+    n2 = float(input('Digite a 2ª nota: '))
+    n3 = float(input('Digite a 3ª nota: '))
+
+    media = (n1 + n2 + n3) / 3
+
+    print(f'\nNome do aluno: {nome}')
+    print(f'Média do aluno: {media:.1f}')
+
+    if media < 5:
+        reprovados += 1
+        print('Reprovado')
+
+    elif media < 7:
+        recuperacao += 1
+        print('Recuperação')
+
+    elif media < 9:
+        aprovados += 1
+        print('Aprovado')
+
+    else:
+        excelentes += 1
+        print('Você foi excelente!')
+
+    print('\n===== RELATÓRIO =====')
+    print(f'Excelentes: {excelentes}')
+    print(f'Aprovados: {aprovados}')
+    print(f'Recuperação: {recuperacao}')
+    print(f'Reprovados: {reprovados}')
+
+    while True:
+        cont = input('\nDeseja continuar? (s/n): ').lower()
+
+        if cont == 's':
+            break
+
+        elif cont == 'n':
+            print('\nPrograma encerrado.')
+            exit()
+
+        else:
+            print('Opção inválida! Tente novamente.')
