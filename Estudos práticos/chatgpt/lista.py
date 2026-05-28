8dc018e460bbbafb29bc5ef8e667cc12f1ab2071
Jonathan <jonathanalmeidadk2@gmail.com>
2026-05-27 22:45:33 -0300

    listas

--- Estudos práticos/chatgpt/lista.py
+++ Estudos práticos/chatgpt/lista.py
@@ -1,5 +1,5 @@
 '''
-
+# Exercício 1 — Lista de frutas 🍎
 lista_frutas = ['Banana', 'Maçã', 'Pera', 'Morango', 'Uva']
 print(lista_frutas)
 print(lista_frutas[0])
@@ -7,9 +7,15 @@ print(lista_frutas[4])
 
 '''
 
+'''
+# Exercício 2 — Adicionando nomes 👾
 nomes = []
 
 for n in range(5):
     nome = input('Digite um nome: ')
     nomes.append(nome)
 print(nomes)
+
+'''
+
+# Exercício 3 — Soma de números 🔥
