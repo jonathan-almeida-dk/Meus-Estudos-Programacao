852bae1f1255c72349769efc75452e71daabbe97
Jonathan <jonathanalmeidadk2@gmail.com>
2026-05-27 22:44:54 -0300

    Listas

---
+++ Estudos práticos/chatgpt/lista_frutas.py
@@ -0,0 +1,15 @@
+'''
+
+lista_frutas = ['Banana', 'Maçã', 'Pera', 'Morango', 'Uva']
+print(lista_frutas)
+print(lista_frutas[0])
+print(lista_frutas[4])
+
+'''
+
+nomes = []
+
+for n in range(5):
+    nome = input('Digite um nome: ')
+    nomes.append(nome)
+print(nomes)
