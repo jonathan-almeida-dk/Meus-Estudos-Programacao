e8a250f48333cbe7908dce98e7027234f8f66c40
Jonathan <jonathanalmeidadk2@gmail.com>
2026-06-07 19:12:50 -0300

    dia 2

---
+++ Estudos práticos/Estudos Lógica/dia 2.py
@@ -0,0 +1,8 @@
+# Mostrar apenas números pares de 1 a 20.
+# Escreva o algoritmo em português primeiro.
+
+for n in range(1,21):
+    if n % 2 == 0:
+        print(n)
+# em uma range de 1 à 21 em que 'n' é cada numero,
+# se a divisão entre 'n' e 2 for igual a '0'(se for par), print este número.
--- Estudos práticos/Estudos Lógica/dia_1.py
+++ Estudos práticos/Estudos Lógica/dia_1.py
@@ -6,3 +6,6 @@
 # • Quais passos executar?
 for n in range(1,11):
     print(n)
+# explicação: 'n' é cada número dentro da range entre 1,10(o 11 não conta)
+# logo, quando se der print em 'n', ele percorre a lista de numeros dentro da range,
+# mostrando um em seguida do outro.
