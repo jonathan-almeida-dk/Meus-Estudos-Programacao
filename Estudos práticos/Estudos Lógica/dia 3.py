1b47b763e842607745fe05c370bd3fe4f6af062b
Jonathan <jonathanalmeidadk2@gmail.com>
2026-06-07 19:56:15 -0300

    adicionando comentários

--- Estudos práticos/Estudos Lógica/dia 3.py
+++ Estudos práticos/Estudos Lógica/dia 3.py
@@ -5,8 +5,8 @@
 
 soma = 0
 
-for n in range(1,6):
-    numero = int(input(f'Digite o {n}º número: '))
-    soma += numero
-media = soma / 5
+for n in range(1,6):                               # 'n' percorre a range de 1 à 6
+    numero = int(input(f'Digite o {n}º número: ')) # pergunta 5 vezes um numero
+    soma += numero                                 # variável soma recebe o valor dela mais o número dito pelo usuário cada vez que é perguntado ao mesmo
+media = soma / 5                                   # divide o valor final entre soma e 5 e descobre a média
 print(f'A média destes números é {media:.1f}.')
--- Estudos práticos/Estudos Lógica/dia 4.py
+++ Estudos práticos/Estudos Lógica/dia 4.py
@@ -1,8 +1,8 @@
 # Dia 4
 # Descobrir o maior número de uma lista.
 
-lista = [5,10,20,45,20,1]
-maior = max(lista)
-print(lista)
-print(f'O maior número da lista é o {maior}')
+lista = [5,10,20,45,20,1]                     # lista criada
+maior = max(lista)                            # variável 'maior' recebe o maior número da lista
+print(lista)                                  # printa a lista
+print(f'O maior número da lista é o {maior}') # printa o maior número da lista
 
