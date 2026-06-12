3f0b634e8d661666ea365756b523e7dba61a77f2
Jonathan <jonathanalmeidadk2@gmail.com>
2026-06-11 22:55:23 -0300

    super progressao

---
+++ Estudos práticos/curso em video2/super_progressao.py
@@ -0,0 +1,20 @@
+
+print('=-' * 10)
+print('GERADOR DE PA'.center(20))
+print('=-' * 10)
+
+termo = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while cont <= total:
+            print(f' {termo}➡️ ', end='')
+            termo += razao
+            cont += 1
+    print(' PAUSA')
+    mais = int(input('\nQuantos termos você quer mostrar a mais?: '))
+print(f'Total de progressões de {total} termos.')
+
