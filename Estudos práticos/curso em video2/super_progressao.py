2008b12b31dd07cc7590fe54e59244d5bd05c4d5
Jonathan <jonathanalmeidadk2@gmail.com>
2026-06-12 00:03:51 -0300

    update

--- Estudos práticos/curso em video2/super_progressao.py
+++ Estudos práticos/curso em video2/super_progressao.py
@@ -6,16 +6,16 @@ print('=-' * 10)
 termo = int(input('Primeiro termo: '))
 razao = int(input('Razão da PA: '))
 
-cont = 1 #contador para impor um limite de termos na primeira vez
-total = 0
-mais = 10
-while mais != 0:
-    total += mais
-    while cont <= total:
+cont = 1                  # contador de termos
+total = 0                 # variável para impor limite de termos por vez 
+mais = 10                 # variável inicial com 10 termos 
+
+while mais != 0:          # enquanto 'mais' for diferente de '0' faça isso:
+    total += mais         # 'total' recebe o valor da variável 'mais' para iniciar com 10 termos e contar quantos termos foram feitos no total ao fim do programa
+    while cont <= total:  # enquanto o contador for menor ou igual ao total:
         print(f'{termo}➡️', end='  ')
-        termo += razao
-        cont += 1
+        termo += razao    # termo é atualizado somando com a razão
+        cont += 1         # contador
     print('PAUSA')
     mais = int(input('\nQuantos termos você quer mostrar a mais?: '))
 print(f'Total de progressões de {total} termos.')
-
