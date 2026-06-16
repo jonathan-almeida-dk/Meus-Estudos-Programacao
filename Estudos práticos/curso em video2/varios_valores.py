a7c719615de53005278735fe1b699240ad6b176d
Jonathan <jonathanalmeidadk2@gmail.com>
2026-06-15 22:39:03 -0300

    varios valores

---
+++ Estudos práticos/curso em video2/varios_valores.py
@@ -0,0 +1,10 @@
+print('-'*30)
+print('TRATANDO VÁRIOS VALORES')
+print('-'*30)
+
+vn = 0
+total = 0
+while vn != 999:
+    vn = int(input('Digite um número: '))
+    total += vn
+print(f'A soma total é {total-999}.')
