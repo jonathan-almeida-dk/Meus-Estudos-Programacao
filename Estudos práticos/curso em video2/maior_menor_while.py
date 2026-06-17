56112fae664aeca7f61f05e83c528b6063b0c2b9
Jonathan <jonathanalmeidadk2@gmail.com>
2026-06-16 22:20:01 -0300

    maior_menor_while

---
+++ Estudos práticos/curso em video2/maior_menor_while.py
@@ -0,0 +1,36 @@
+''' Exercício Python 65:
+    Crie um programa que leia vários números inteiros pelo teclado. 
+    No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos.
+    O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+'''
+
+lista_numeros = []
+
+soma = quantidade = media = maior = menor = 0
+pergunta = 'S'
+
+while pergunta == 'S':
+    numeros = int(input('Digite um número: '))
+    print('='*25)
+    lista_numeros.append(numeros)
+    soma += numeros
+    quantidade += 1
+    if quantidade == 1:
+        maior = menor = numeros
+    else:
+        if numeros > maior:
+            maior = numeros
+        if numeros < menor:
+            menor = numeros
+    pergunta = input('Quer continuar? [S/N] ').strip().upper()[0]
+    print('='*25)
+
+media = soma / quantidade
+print('='*25)
+print(f'Você digitou {quantidade} números.')
+print('='*25)
+print(f'Maior número é * {maior} *.')
+print('='*25)
+print(f'Menor número é * {menor} *.')
+print('='*25)
+print(f'A média dos números é * {media} *.')
--- Estudos práticos/curso em video2/varios_valores.py
+++ Estudos práticos/curso em video2/varios_valores.py
@@ -2,9 +2,9 @@ print('-'*30)
 print('TRATANDO VÁRIOS VALORES')
 print('-'*30)
 
-vn = 0
-total = 0
-while vn != 999:
-    vn = int(input('Digite um número: '))
-    total += vn
-print(f'A soma total é {total-999}.')
+num = cont = soma = 0
+while num != 999:
+    num = int(input('Digite um número: '))
+    soma += num
+    cont +=1
+print(f'Você digitou {cont -1} vezes. A soma total é {soma - 999}.')
