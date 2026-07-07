4df49f6257de54308808803f99ec0e9f56f643ee
Jonathan <jonathanalmeidadk2@gmail.com>
2026-07-06 21:33:18 -0300

    melhor resposta

--- Estudos práticos/curso em video3/lista_preços_tuplas.py
+++ Estudos práticos/curso em video3/lista_preços_tuplas.py
@@ -2,14 +2,18 @@
 # de produtos e seus respectivos preços, na sequência. 
 # No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 
-produtos = ('Arroz......................R$ ', 14.96, 'Feijão.....................R$ ', 20.50, 'Mostarda...................R$ ', 5.98,
-            'Macarrão...................R$ ',3.95, 'Refrigerante...............R$ ', 9.98, 'Maionese...................R$ ', 11.95)
+produtos = ('Arroz', 14.96, 
+            'Feijão', 20.50, 
+            'Mostarda', 5.98,
+            'Macarrão', 3.95, 
+            'Refrigerante', 9.98, 
+            'Maionese', 11.95)
 print('-='*20)
 print('Listagens de Produtos'.center(40))
 print('-='*20)
-print(produtos[0],produtos[1])
-print(produtos[2],produtos[3])
-print(produtos[4],produtos[5])
-print(produtos[6],produtos[7])
-print(produtos[8],produtos[9])
-print(produtos[10],produtos[11])
+for pos in range(0, len(produtos)):
+    if pos % 2 == 0:
+        print(f'{produtos[pos]:.<30}', end = '')
+    else:
+        print(f'R${produtos[pos]:>7.2f}')
+print('-='*20)
