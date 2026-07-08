e47cbadf81d95d129abb2eee09fa200bceb68c99
Jonathan <jonathanalmeidadk2@gmail.com>
2026-07-07 22:49:46 -0300

    resolução do problema

--- Estudos práticos/curso em video3/maior_menor_lista.py
+++ Estudos práticos/curso em video3/maior_menor_lista.py
@@ -2,9 +2,23 @@
 # No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
 lista = []
 for n in range(0,5):
-    lista.append(int(input('Digite um valor: ')))
+    lista.append(int(input(f'Digite um valor para a posição {n}: ')))
+    if n == 0: # SE FOR O 1º NÚMERO DIGITADO E FOR IGUAL A 0:
+        maior = menor = lista[n] # ELE SERÁ CONSIDERADO MAIOR E MENOR NÚMERO
+    else:
+        if lista[n] > maior: # se o numero da lista for maior que 'maior numero':
+            maior = lista[n] # o 'maior numero' recebe o valor do numero da lista
+        if lista[n] < menor: # se o numero da lista for menor que 'menor numero':
+            menor = lista[n] # o 'menor numero' recebe o valor do numero da lista
 print(lista)
-if max(lista) in lista:
-    print(f'O maior valor foi {max(lista)} e está na posição {lista.index(max(lista))+1}')
-if min(lista) in lista:
-    print(f'O menor valor foi {min(lista)} e está na posição {lista.index(min(lista))+1}')
+print('='*40)
+print(f'O maior valor foi {maior} e está na posição ',end='')
+for i, v in enumerate(lista):
+    if v == maior:
+        print(f'{i}...', end='')
+print()
+print(f'O menor valor foi {menor} e está na posição ', end='')
+for i, v in enumerate(lista):
+    if v == menor:
+        print(f'{i}...', end='')
+print()
