00379d9f8193fffeec26bca608c480a23561d4f7
Jonathan <jonathanalmeidadk2@gmail.com>
2026-07-15 22:25:51 -0300

    correção

--- Estudos práticos/curso em video3/extraindo_dados.py
+++ Estudos práticos/curso em video3/extraindo_dados.py
@@ -5,30 +5,17 @@
 #                 C) Se o valor 5 foi digitado e está ou não na lista.
 
 lista=[]
-
 while True:
-    n = int(input('Digite um valor: '))
-    lista.append(n)
-    continuar = input('Deseja continuar? [S/N] ')
-    if continuar in 'Nn':  
-        print('='*40)
-        opções = int(input('Escolha uma das opções:' \
-        '\n1 - Quantos números foram digitados' \
-        '\n2 - Lista em ordem decrescente' \
-        '\n3 - Existe o valor 5 na lista?' \
-        '\n4 - Encerrar programa\nResposta: '))
-        if opções == 1:
-            print('='*40)
-            print(f'Quantidade de números digitados foram {len(lista)}')
-            break
-        elif opções == 2:
-            print('='*40)
-            print(f'Itens da lista ordenada de forma decrescente {sorted(lista, reverse=True)}')
-            break
-        elif opções == 3:
-            if 5 in lista:
-                print(f'Sim, existe o valor 5 na lista e está na posição {lista.index(5) + 1}')
-                break
-        if opções == 4:
-            break
-print('='*40)
+    lista.append(int(input('Digite um valor: ')))
+    resp = input('Quer continuar? [S/N] ')
+    if resp in 'Nn':
+        break
+print('='*30)
+print(f'Você digitou {len(lista)} elementos.')
+lista.sort(reverse=True)
+print(f'Os valores em ordem decrescente são {lista}')
+if 5 in lista:
+    print('O valor 5 faz parte da lista.')
+else:
+    print('O valor 5 não foi encontrado na lista.')
+ 
