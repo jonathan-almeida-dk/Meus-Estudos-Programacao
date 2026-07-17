16bf380c7fd4e1bf1b2d498e7606e4a94817e1ae
Jonathan <jonathanalmeidadk2@gmail.com>
2026-07-16 21:01:45 -0300

    Solução

--- Estudos práticos/curso em video3/divisao_valores_listas.py
+++ Estudos práticos/curso em video3/divisao_valores_listas.py
@@ -6,12 +6,15 @@ números = []
 pares = []
 ímpares = []
 while 0 not in números:
-    num = (int(input('Digite um número: ')))
-    números.append(num)
-    if num % 2 == 0:
-        pares.append(num)
-    else:
-        ímpares.append(num)
+    números.append(int(input('Digite um número: ')))
+    resp = input('Quer continuar? [S/N] ')
+    if resp in 'Nn':
+        break
+for i, v in enumerate(números):
+        if v % 2 == 0:
+            pares.append(v)
+        else:
+            ímpares.append(v)
 print('Lista: ',números)
 print('Pares: ',pares)
 print('Ímpares: ',ímpares)
