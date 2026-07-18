5a87fae85b84e4a2b5ce6ed4882b2327c2a3f4ba
Jonathan <jonathanalmeidadk2@gmail.com>
2026-07-17 22:58:56 -0300

    aula prática

--- Estudos práticos/curso em video3/lista_2v0.py
+++ Estudos práticos/curso em video3/lista_2v0.py
@@ -8,13 +8,13 @@ dados.append(32)
 
 print(dados[0]) # Pedro
 print(dados[1]) # 25
+print('-='*60)
 
 #====================================
 
-# pessoas = []
-# pessoas.append(dados[:]) # adiciona uma lista dentro da outra com todos os itens(dá pra fazer várias listas dentro de uma)
+pessoas = []
 
-# OU
+pessoas.append(dados[:]) # cria um cópia com [:] e  adiciona essa cópia da lista, dentro da outra com todos os itens(dá pra fazer várias listas dentro de uma)
 
 pessoas = [['Pedro', 25], ['Maria',19], ['João', 32]]
 # CADA LISTA VAI TER 1 ÍNDICE(  << PEDRO 25 = ÍNDICE '0' >>   << MARIA 19 = ÍNDICE '1' >>     << JOÃO 32 = ÍNDICE '2' >>  )
@@ -22,3 +22,20 @@ print(pessoas[0][0]) # Pedro
 print(pessoas[1][1]) # 19
 print(pessoas[2][0]) # João
 print(pessoas[1])    # ['Maria', 19]
+print('-='*60)
+
+#====================================
+
+for p in pessoas:
+    print(f'{p[0]} tem {p[1]} anos')
+
+#====================================
+
+galera = []
+dado = []
+for c in range(0,3):
+    dado.append(input('Nome: '))
+    dado.append(input('Idade: '))
+    galera.append(dado[:])
+    dado.clear()
+print(galera)
