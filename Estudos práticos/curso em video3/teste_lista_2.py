a4016dd6b43027e71fdaf3c5bb1bbbafeb95867c
Jonathan <jonathanalmeidadk2@gmail.com>
2026-07-20 20:52:19 -0300

    teste_lista2

--- Estudos práticos/curso em video3/teste_lista_2.py
+++ Estudos práticos/curso em video3/teste_lista_2.py
@@ -35,7 +35,7 @@ galera = []
 dado = []
 for c in range(0,3):
     dado.append(input('Nome: '))
-    dado.append(input('Idade: '))
+    dado.append(int((input('Idade: '))))
     galera.append(dado[:])
     dado.clear()
 print(galera)
