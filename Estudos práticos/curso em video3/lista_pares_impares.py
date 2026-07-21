a2586f3df2db295a9e96525189ae69dd88363986
Jonathan <jonathanalmeidadk2@gmail.com>
2026-07-20 21:40:11 -0300

    solução correta

--- Estudos práticos/curso em video3/lista_pares_impares.py
+++ Estudos práticos/curso em video3/lista_pares_impares.py
@@ -2,18 +2,15 @@
 # e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final,
 # mostre os valores pares e ímpares em ordem crescente.
 
-lista =[]
-pares = []
-impares = []
-for n in range(1,8):
-    lista.append(int(input('Digite um número: ')))
-    if n % 2 == 0:
-        pares.append(n)
-        pares.sort()
+numeros = [[],[]]
+for c in range(1,8):
+    valor = (int(input(f'Digite o {c}º número: ')))
+    if valor % 2 == 0:
+        numeros[0].append(valor)
     else:
-        impares.append(n)
-        impares.sort()
-lista.sort()
-print(lista)
-print(f'Lista de números pares {pares}')
-print(f'Lista de números ímpares {impares}')
+        numeros[1].append(valor)
+print('='*45)
+numeros[0].sort()
+numeros[1].sort()
+print(f'Todos os valores pares: {numeros[0]}')
+print(f'Todos os valores ímpares: {numeros[1]}')
