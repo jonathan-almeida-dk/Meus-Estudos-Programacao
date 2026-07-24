78710b7774a51e10f97c5f6219bd7859cf83b543
Jonathan <jonathanalmeidadk2@gmail.com>
2026-07-23 22:46:14 -0300

    solução boletim

--- Estudos práticos/curso em video3/boletim_listas.py
+++ Estudos práticos/curso em video3/boletim_listas.py
@@ -2,21 +2,29 @@
 # alunos e guarde tudo em uma lista composta. No final, mostre um boletim contendo
 # a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.
 
-info_notas = []
-dados = []
+ficha = []
 while True:
-    nome = dados.append(input('Digite o nome: '))
-    n1 = dados.append(float(input(f'Digite a 1ª nota: ')))
-    n2 = dados.append(float(input(f'Digite a 2ª nota: ')))
-    info_notas.append(dados[:])
-    dados.clear()
+    nome = (input('Digite o nome: '))
+    n1 = (float(input(f'Digite a 1ª nota: ')))
+    n2 = (float(input(f'Digite a 2ª nota: ')))
+    media = (n1 + n2) / 2
+    ficha.append([nome, [n1,n2], media])
     resp = input('Quer continuar?: ')
     if resp in 'Nn':
-        print('-'*30)
-        print(' BOLETIM '.center(30))
-        print('-'*30)
-        for n in range(1):
-            print('Aluno: ',info_notas[0][0])
-            media = (info_notas[0][1] + info_notas[0][2]) / 2
-            print('Média: ',media)
         break
+
+print('-='*30)
+print(f'{"N°":<4}{'Nome':<10}{'Média':>8}')
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+    
+while True:
+    
+    opc = int(input('Mostrar notas de qual aluno? (999 interrompe:)'))
+    if opc == 999:
+        print('FINZALIZANDO...')
+        break
+    
+    if opc<= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}]')
+print('Volte sempre!')
