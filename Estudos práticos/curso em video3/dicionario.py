7199efce19a8a97960fd0eec5ce9e38267d1465e
Jonathan <jonathanalmeidadk2@gmail.com>
2026-07-28 22:51:51 -0300

    Resolução dicionario.py

--- Estudos práticos/curso em video3/dicionario.py
+++ Estudos práticos/curso em video3/dicionario.py
@@ -1,15 +1,14 @@
 # Exercício Python 090: Faça um programa que leia nome e média de um aluno,
 # guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.
 alunos = {}
-
 alunos['Nome'] = input('Nome: ')
 alunos['Média'] = float(input(f'Média de {alunos["Nome"]}: '))
-print('-='*30)
-print(f'O nome é igual a {alunos["Nome"]}')
-print(f'A média é igual a {alunos["Média"]}')
 if alunos["Média"] >= 8:
-    print(f'A situação é igual a Aprovado.')
-elif alunos["Média"] >=6:
-    print(f'A situação é igual a Recuperação.')
-elif alunos["Média"] < 6:
-    print(f'A situação é igual a Reprovado')
+    alunos['Situação'] = 'Aprovado'
+elif 6 <= alunos["Média"] <8:
+    alunos['Situação'] = 'Recuperação'
+else:
+    alunos['Situação'] = 'Reprovado'
+print('-='*30)
+for k, v in alunos.items():
+    print(f' - {k} é igual a {v}')
