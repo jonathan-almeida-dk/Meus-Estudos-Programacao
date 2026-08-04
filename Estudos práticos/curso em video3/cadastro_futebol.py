54da3fa184bc955c8040110912b364da7b384d41
Jonathan <jonathanalmeidadk2@gmail.com>
2026-08-03 21:34:43 -0300

    cadastro_futebol resolvido

--- Estudos práticos/curso em video3/cadastro_futebol.py
+++ Estudos práticos/curso em video3/cadastro_futebol.py
@@ -11,17 +11,23 @@ info['Nome'] = input('Nome do Jogador: ')
 partidas = int(input(f'Quantas partidas {info["Nome"]} jogou? '))
 while True:
     if partidas != 0:
-        for c in range(1,partidas+1):
-            gol = (int(input(f'Quantos gols ele fez na {c}ª partida? ')))
+        for c in range(0,partidas):
+            gol = (int(input(f'Quantos gols ele fez na {c+1}ª partida? ')))
             gols.append(gol)
             totgols += gol
         info['Gols'] = gols
         break
 info['Total de Gols'] = totgols
+print('-='*30)
 
 # DEMOSTRAÇÕES:
 print(info)
-
+print('-='*30)
 for k, v in info.items():
     print(f'O campo {k} tem o valor {v}.')
+print('-='*30)
 
+print(f'O jogador {info["Nome"]} jogou {partidas} partidas.')
+for i, v in enumerate(info['Gols']):
+    print(f'    Na partida {i+1}, fez {v} gols.')
+print(f'Foi um total de {info['Total de Gols']} gols.')
