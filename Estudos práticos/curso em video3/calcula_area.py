1c5141d99e60bcf0a121ab04a06a282e8c1213c1
Jonathan <jonathanalmeidadk2@gmail.com>
2026-08-07 22:23:52 -0300

    solução calcula area

--- Estudos práticos/curso em video3/calcula_area.py
+++ Estudos práticos/curso em video3/calcula_area.py
@@ -1,7 +1,12 @@
 # Exercício Python 096: Faça um programa que tenha uma função chamada área(),
 # que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.
-def área():
-    larg = float(input('LARGURA (m): '))
-    comp = float(input('COMPRIMENTO (m): '))
-    print(f'A área de um terreno de {larg}m x {comp}m é de {larg*comp}m².')
-área()
+def área(larg, comp):
+    a = larg * comp
+    print(f'A área de um terreno de {larg}m x {comp}m é de {a}m².')
+
+# Programa principal
+print(' Controle de Terrenos ')
+print('-'*20)
+l = float(input('LARGURA (m): '))
+c = float(input('COMPRIMENTO (m): '))
+área(l,c)
