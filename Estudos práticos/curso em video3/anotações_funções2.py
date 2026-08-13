5ff45abd5a0b56fe44304918f8b6fa02858ce011
Jonathan <jonathanalmeidadk2@gmail.com>
2026-08-12 23:13:21 -0300

    anotações funções parte2

--- Estudos práticos/curso em video3/anotações_funções2.py
+++ Estudos práticos/curso em video3/anotações_funções2.py
@@ -58,4 +58,36 @@ r2 = somar(1,7)
 r3 = somar(4)
 print(f'Meus cálculos deram {r1}, {r2} e {r3}.')
 print('-='*30)
+
+# -------------------------------------------
+
+def fatorial(num=1):
+    f = 1
+    for c in range(num, 0, -1):
+        f*=c
+    return f
+n = int(input('Digite um número: '))
+print(f'O fatorial de {n} é igual a {fatorial(n)}')
+print('='*30)
+f1 = fatorial(5)
+f2 = fatorial(4)
+f3 = fatorial()
+print(f'Os resultados são {f1}. {f2} e {f3}')
+
+print('-='*30)
+# -------------------------------------------
+
+# FUNÇÃO PAR OU ÍMPAR
+
+def par(n=0):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+num = int(input('Digite um número: '))
+if par(num):
+    print('É par!')
+else:
+    print('É ímpar!')
+
 # -------------------------------------------
