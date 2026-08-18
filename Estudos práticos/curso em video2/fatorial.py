707b1643e4f1b7fe0b7c77833f9560460cda29a6
Jonathan <jonathanalmeidadk2@gmail.com>
2026-08-17 23:04:27 -0300

    função fatorial

--- Estudos práticos/curso em video2/fatorial.py
+++ Estudos práticos/curso em video2/fatorial.py
@@ -3,10 +3,10 @@
 # 5! = 5 x 4 x 3 x 2 x 1 = 120
 
 
-# from math import factorial
-# n = int(input('Digite um número para calcular seu fatorial: '))
-# f = factorial(n)
-# print(f'O fatorial de {n} é {f}.')
+from math import factorial
+n = int(input('Digite um número para calcular seu fatorial: '))
+f = factorial(n)
+print(f'O fatorial de {n} é {f}.')
 
 #or
 
