338b80ef8db74d933490741cd990b205fb405085
Jonathan <jonathanalmeidadk2@gmail.com>
2026-08-19 22:29:10 -0300

    ficha_jogador

--- Estudos práticos/curso em video3/ficha_jogado.py
+++ Estudos práticos/curso em video3/ficha_jogado.py
@@ -3,14 +3,17 @@
 # O programa deverá ser capaz de mostrar a ficha do jogador,
 # mesmo que algum dado não tenha sido informado corretamente.
 
-def ficha(jog = '<desconhecido>', gols = '0'):
-    jog = str(input('Nome do jogador: '))
-    gols = str(input('Número de gols: '))
-    if jog in '<desconhecido>':
-        jog = '<desconhecido>'
-    if gols in '':
-        gols = 0
+def ficha(jog = '<desconhecido>', gols = 0):
     print(f'O jogador {jog} fez {gols} gol(s) no campeonato')
 
 # Programa Principal
-ficha(gols=0)
+n = str(input('Nome do jogador: '))
+g = str(input('Número de gols: '))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':
+    ficha(gols = g)
+else:
+    ficha(n,g)
