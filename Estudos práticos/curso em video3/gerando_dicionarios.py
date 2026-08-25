72f65a89df2d4ba0913123606f793e84fab36adb
Jonathan <jonathanalmeidadk2@gmail.com>
2026-08-24 22:59:12 -0300

    gerando dicionarios

---
+++ Estudos práticos/curso em video3/gerando_dicionarios.py
@@ -0,0 +1,31 @@
+# Exercício Python 105: Faça um programa que tenha uma função notas()
+# que pode receber várias notas de alunos e vai retornar
+# um dicionário com as seguintes informações:
+    # – Quantidade de notas
+    # – A maior nota
+    # – A menor nota
+    # – A média da turma 
+    # – A situação (opcional)
+
+def notas(*n, sit = False):
+    alunos_notas = {}
+
+    alunos_notas['total'] = len(n)
+    alunos_notas['maior'] = max(n)
+    alunos_notas['menor'] = min(n)
+    alunos_notas['média'] = sum(n) / len(n)
+
+    if sit:
+        if alunos_notas['média'] >= 7:
+            alunos_notas['situação'] = 'BOA'
+        elif alunos_notas['média'] >= 5:
+            alunos_notas['situação'] = 'RAZOÁVEL'
+        else:
+            alunos_notas['situação'] = 'RUIM'
+            
+    return alunos_notas
+
+
+# Programa Principal
+resp = notas(5.5,9.5,1,6.5,sit=True)
+print(resp)
