7d1fc9c9287bde52f32717ec147f76ed6ff1e47a
Jonathan <jonathanalmeidadk2@gmail.com>
2026-08-31 23:12:59 -0300

    resultado final moeda.py

--- Estudos práticos/Aulas de módulos/moedas/__init__.py
+++ Estudos práticos/Aulas de módulos/moedas/__init__.py
@@ -3,5 +3,5 @@ import moeda
 num = float(input('Digite o preço: '))
 print(f'A metade de R${num:.2f} é R${moeda.metade(num):.2f}')
 print(f'O dobro de R${num:.2f} é R${moeda.dobro(num):.2f}')
-print(f'Aumentando 10%, temos R${moeda.aumentar(num):.2f}')
-print(f'Diminuindo 10%, temos R${moeda.diminuir(num):.2f}')
+print(f'Aumentando 10%, temos R${moeda.aumentar(num,10):.2f}')
+print(f'Diminuindo 10%, temos R${moeda.diminuir(num,10):.2f}')
