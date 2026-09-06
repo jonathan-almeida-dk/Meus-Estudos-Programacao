0b72e15b237362e74621b4ec501c960a06cf9429
Jonathan <jonathanalmeidadk2@gmail.com>
2026-09-06 15:57:09 -0300

    diminuindo código principal moedas(moeda.py)

--- Estudos práticos/Aulas de módulos/moedas/moeda.py
+++ Estudos práticos/Aulas de módulos/moedas/moeda.py
@@ -10,6 +10,10 @@
 # para que elas aceitem um parâmetro a mais, informando se o valor retornado
 # por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
 
+# Exercício Python 110: Adicione o módulo moeda.py criado nos desafios anteriores,
+# uma função chamada resumo(), que mostre na tela algumas informações geradas
+# pelas funções que já temos no módulo criado até aqui.
+
 def diminuir(preço=0, taxa=0, formato=False):
     res = preço - (preço * taxa/100)
     return res if formato is False else moeda(res)
@@ -29,3 +33,15 @@ def metade(preço=0, formato=False):
 
 def moeda(preço=0, moeda='R$'):
     return f'{moeda}{preço:.2f}'.replace('.',',')
+
+def resumo(preço=0, taxaa=10, taxar=5):
+    print('-'*30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-'*30)
+    print(f'Preço analisado: \tR${moeda(preço) }')
+    print(f'Dobro do preço: \tR${dobro(preço, True) }')
+    print(f'Metade do preço: \tR${metade(preço, True) }')
+    print(f'{taxaa}% de aumento: \tR${aumentar(preço, taxaa, True) }')
+    print(f'{taxar}% de redução: \tR${diminuir(preço, taxaa, True) }')
+    print('-'*30)
+
