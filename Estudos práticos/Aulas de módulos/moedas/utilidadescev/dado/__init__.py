2e7b9ccfc62cef15101133a32887c3ac352cb7b4
Jonathan <jonathanalmeidadk2@gmail.com>
2026-09-08 23:06:50 -0300

    entrada de dados monetários

--- Estudos práticos/Aulas de módulos/moedas/utilidadescev/dado/__init__.py
+++ Estudos práticos/Aulas de módulos/moedas/utilidadescev/dado/__init__.py
@@ -1,8 +1,8 @@
 def leiaDinheiro(msg):
     válido = False
     while not válido:
-        entrada = str(input(msg)).replace(',', '.')
-        if entrada.isalpha():
+        entrada = str(input(msg)).replace(',', '.').strip()
+        if entrada.isalpha() or entrada.strip() == '':
             print(f'\033[0;31mERRO: \"{entrada}\" é um preço inválido!\033[m')
         else:
             válido = True
