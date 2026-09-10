a474d8c580c8754be0c878ac12ea6278769f7013
Jonathan <jonathanalmeidadk2@gmail.com>
2026-09-09 21:13:11 -0300

    funções com tratamento de erros

---
+++ Estudos práticos/Tratamento de Erros/Funcoes_aprofundadas.py
@@ -0,0 +1,34 @@
+# Exercício Python 113: Reescreva a função leiaInt() que fizemos no desafio 104,
+# incluindo agora a possibilidade da digitação de um número de tipo inválido.
+# Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.
+
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO: por favor, digite um número inteiro válido!\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+
+def leiafloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO: por favor, digite um número inteiro válido!\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+
+# Programa Principal
+n1 = leiaInt('Digite um Inteiro: ')
+n2 = leiafloat('Digite um Real: ')
+print(f'O valor digitado inteiro foi {n1} e o real foi {n2}')
